75479e8ada16a0b26652c32cc439a3e7edf4cdf1
bulutluoz <abbulutluoz@gmail.com>
2024-04-05 10:39:36 +0200

    12.gun sonu

---
+++ src/test/java/tests/day12_testNGFramework/C04_CheckboxTesti.java
@@ -0,0 +1,15 @@
+package tests.day12_testNGFramework;
+
+import org.testng.annotations.Test;
+
+public class C04_CheckboxTesti {
+
+    @Test
+    public void test01(){
+        //1 - https://testotomasyonu.com/form adresine gidin
+        //2- Sirt Agrisi ve Carpinti checkbox’larini secin
+        //3- Sirt Agrisi ve Carpinti checkbox’larininin seçili olduğunu test edin
+        //4- Seker ve Epilepsi checkbox’larininin seçili olmadigini test edin
+        //5- sayfayi kapatin
+    }
+}
