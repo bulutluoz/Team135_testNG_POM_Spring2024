1a29d2170cb6d1e823a50bee3cbb086edc519638
bulutluoz <abbulutluoz@gmail.com>
2024-04-08 10:13:15 +0200

    13.gun sonu

--- src/test/java/tests/day13_TestNG_Assertions/C02_UrunSepetiTesti.java
+++ src/test/java/tests/day13_TestNG_Assertions/C02_UrunSepetiTesti.java
@@ -1,12 +1,19 @@
 package tests.day13_TestNG_Assertions;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.TestOtomasyonuPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+
+import java.time.Duration;
 
 public class C02_UrunSepetiTesti {
 
@@ -22,12 +29,14 @@ public class C02_UrunSepetiTesti {
         //4- urun ismini kaydedin ve sepete ekleyin
         String secilenUrunIsmi = testOtomasyonuPage.urunIsimElementi.getText();
         testOtomasyonuPage.addToCartButonu.click();
-        ReusableMethods.bekle(4);
+        ReusableMethods.bekle(3);
+
         //5- your cart linkine tiklayin
         testOtomasyonuPage.yourCartLinki.click();
+
         //6- kaydettiginiz urun ismi ile sepetteki urun isminin ayni oldugunu test edin
         String sepettekiUrunIsmi = testOtomasyonuPage.sepettekiUrunIsimElementi.getText();
-        Assert.assertEquals(secilenUrunIsmi,sepettekiUrunIsmi);
+        Assert.assertTrue(secilenUrunIsmi.equalsIgnoreCase(sepettekiUrunIsmi));
         //7- sayfayi kapatin
         Driver.quitDriver();
     }
