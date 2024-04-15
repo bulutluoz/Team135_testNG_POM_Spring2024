599b154169d89ddc19a6f6af1d9b9ed5b6efec88
bulutluoz <abbulutluoz@gmail.com>
2024-04-15 09:38:18 +0200

    14.gun sonu

---
+++ src/test/java/tests/day14_xmlDosyaKullanimi/C01_TestHiyerarsisi.java
@@ -0,0 +1,27 @@
+package tests.day14_xmlDosyaKullanimi;
+
+public class C01_TestHiyerarsisi {
+    /*
+        Yazilim testi, bir yazilimin baslangicta planlanan amaclara uygun olarak
+        calisip calismadigini denetlemek icin yapilan faaliyetlerin tumudur
+
+        Test Suite ; Bir/birden fazla testi biraraya gelmesiyle olusur
+
+        Test : Belirli bir amac icin biraraya getirilmis,
+               test package, class veya method'laridir
+               Ornek, smoke test ; kullandigimiz uygulama icin en can alici ozelliklerin
+               test edilmesidir.
+
+        test hiyerarsisi: Suite, Test ve sonrasinda package, class, method veya gruplardan olusur
+
+        Ornek olarak
+        bizim smoke testimiz
+           day11 ==> C04
+           day12 ==> C03, C08, C09
+           day13 ==> C01, C07
+
+       Bir test calistirmak istedigimizde
+       hem calistirma, hem de raporlamada kolaylik icin
+       .xml dosyalari kullaniriz
+     */
+}
